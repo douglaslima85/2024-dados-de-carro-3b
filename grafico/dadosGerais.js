3fbec8ff027a3caa6dadabe7219162ea91f85a0a
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2024-10-31 10:40:16 -0300

    Add files via upload

--- grafico/dadosGerais.js
+++ grafico/dadosGerais.js
@@ -5,11 +5,17 @@ async function vizualizainfo() {
     const dados = await res.json()
 
     const carros = Object.keys(dados)
-    const carrosMaisVotado =carros [0]
+    const carrosMaisVotado = carros [0]
     const quantidadeDeVotos = Object.values(dados)[0]
 
-    console.log(carrosMaisVotado)
-    console.log(quantidadeDeVotos)
-}
+    let paragrafo = document.createElement('p')
+    paragrafo.classList.add('caixa-grafico__texto')
+
+    paragrafo.innerHTML = Nessa pesquisa buscou-se compreender qual carros mais vendidos e os mais procurados pelos o brasileiro. O $(carrosMaisVotado) foi o carro mais votado entre todos com um total $(quantidadeDeVotos) em uma pesquisa que teve um total de um milhão de participante. 
+
+    let caixa = document.getElementById
+    ('caixa-grafico')
+    caixa.appendChild(paragrafo)
+} 
 
 vizualizainfo()
