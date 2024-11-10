32afe1f6be1a17fc7061642cae8236a730219dbe
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2024-11-10 11:28:11 -0300

    Add files via upload

--- grafico/dadosGerais.js
+++ grafico/dadosGerais.js
@@ -1,21 +1,22 @@
 const url ='https://raw.githubusercontent.com/douglaslima85/ciencia-de-dados-3B/refs/heads/main/ciencia-de-dados-carros.'
 
-async function vizualizainfo() {
-    const res = await fetch (url)
-    const dados = await res.json()
+async function vizualizaInfo(){
+    const res = await fetch(url)
+    const dados =  await res.json()
 
-    const carros = Object.keys(dados)
-    const carrosMaisVotado = carros [0]
+    const cursos = Object.keys(dados)
+    const carrosMaisVotados = cursos[0]
     const quantidadeDeVotos = Object.values(dados)[0]
 
     let paragrafo = document.createElement('p')
+
     paragrafo.classList.add('caixa-grafico__texto')
 
-    paragrafo.innerHTML = Nessa pesquisa buscou-se compreender qual carros mais vendidos e os mais procurados pelos o brasileiro. O $(carrosMaisVotado) foi o carro mais votado entre todos com um total $(quantidadeDeVotos) em uma pesquisa que teve um total de um milhão de participante. 
+    paragrafo.innerHTML = ` Nessa pesquisa, buscou-se compreender qual o carro  mais procurado pelos jovens brasileiros. O ${carrosMaisVotados} foi o carro mais votado entre todos, com um total de ${quantidadeDeVotos} votos, em uma pesquisa que teve um total de um milhão de participantes.`
 
-    let caixa = document.getElementById
-    ('caixa-grafico')
+    let caixa = document.getElementById('caixa-grafico')
     caixa.appendChild(paragrafo)
-} 
 
-vizualizainfo()
+}
+
+vizualizaInfo()
